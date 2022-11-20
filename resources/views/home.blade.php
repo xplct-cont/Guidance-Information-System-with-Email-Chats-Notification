cad757220279337752971ff2e0064621c1242c97
KennSecusana <kennsecusana@gmail.com>
2022-11-20 16:20:45 +0800

    Committed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,7 @@
 
     <body>
 
+        
         @if ($message = Session::get('status'))
             <div class="alert alert-success alert-block">
                 <button type="button" class="close" data-dismiss="alert" style="color:black;">×</button>
